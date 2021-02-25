322cba6b0c53c9fc56dac2a7e7ec80a074f2632d
Dinmukhammed <iamthegreatthanos@gmail.com>
2021-02-25 16:55:23 +0600

    Color changed.

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,8 +2,8 @@ import 'package:flutter/widgets.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class AppColors {
-  static final Color mainColor = HexColor('2C2B61');
-  static final Color primaryColor = HexColor('D0AE36');
+  static final Color mainColor = HexColor('#2C2B61');
+  static final Color primaryColor = HexColor('#D0AE36');
   
 }
 
