b93f29ebf7284acd911044cb04f2d29dcec72701
sultan841 <seyitmat@mail.ru>
2021-03-05 17:42:44 +0600

    add medication feature and composition pages

--- lib/pages/medication/medication_detail/medication_composition.dart
+++ lib/pages/medication/medication_detail/medication_composition.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pharmacy_aggregator/components/appBar.dart';
+import 'package:pharmacy_aggregator/core/constants.dart';
 
 class MedicationComposition extends StatefulWidget {
   @override
@@ -7,12 +8,27 @@ class MedicationComposition extends StatefulWidget {
 }
 
 class _MedicationCompositionState extends State<MedicationComposition> {
+
+  String word = "Одна таблетка содержит активное вещество - панкреатин** (с минимальной активностью липазы 3 500 ЕД ЕФ, амилазы 4 200 ЕД ЕФ, с минимальной общей активностью протеазы 250 ЕД ЕФ)   вспомогательные вещества: целлюлоза микрокристаллическая, натрия крахмала гликолят (тип А), кремния диоксид коллоидный безводный, магния стеарат,   состав оболочки: полиакрилата 30 % дисперсия в пересчете на сухое вещество, тальк, гипромеллоза, азорубиновый лак (Е 122), эмульсия симетикона, титана диоксид (Е 171), макрогол 6000.   ** - панкреатин (порошок поджелудочных желез)";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: buildAppBar("Описание"),
-      body: Container(),
+      appBar: buildAppBar("Состав"),
+      body:SingleChildScrollView(
+        child: Container(
+          padding:EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(word,
+                style: TextStyle(fontFamily: AppFonts.montesseratRegular, fontSize: 16),),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+
--- lib/pages/medication/medication_detail/medication_feature.dart
+++ lib/pages/medication/medication_detail/medication_feature.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pharmacy_aggregator/components/appBar.dart';
+import 'package:pharmacy_aggregator/core/constants.dart';
 
 class MedicationFeature extends StatefulWidget {
   @override
@@ -11,8 +12,29 @@ class _MedicationFeatureState extends State<MedicationFeature> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: buildAppBar("Состав"),
-      body: Container(),
+      appBar: buildAppBar("Описание"),
+      body: SingleChildScrollView(
+        child: Container(
+          padding:EdgeInsets.all(14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Характеристика", style: TextStyle(fontFamily: AppFonts.montesseratSemiBold, fontSize: 18),),
+              Padding(
+                padding: EdgeInsets.symmetric( vertical: 15),
+                child: Text("Таблетки, покрытые пленочной оболочкой розового цвета, цилиндрической формы, с плоскопараллельной поверхностью, с фаской.",
+                style: TextStyle(fontFamily: AppFonts.montesseratRegular, fontSize: 16),),
+              ),
+              Text("Срок хранения", style: TextStyle(fontFamily: AppFonts.montesseratSemiBold, fontSize: 18),),
+              Padding(
+                padding: EdgeInsets.symmetric( vertical: 15),
+                child: Text("3 года Не применять по истечении срока годности",
+                  style: TextStyle(fontFamily: AppFonts.montesseratRegular, fontSize: 16),),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
