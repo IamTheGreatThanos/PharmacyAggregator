b901a9f2e406e019f8374bba4a0bae8b4d372dcc
Dinmukhammed <iamthegreatthanos@gmail.com>
2021-03-08 13:12:16 +0600

    Count medication page added!

--- lib/pages/medication/medication_detail/medication_count.dart
+++ lib/pages/medication/medication_detail/medication_count.dart
@@ -7,12 +7,39 @@ class MedicationCount extends StatefulWidget {
 }
 
 class _MedicationCountState extends State<MedicationCount> {
+  var array = ['Аптека "Форте+" - 200шт', 'Аптека "Биосфера" - 160шт'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: buildAppBar("Мезим форте таб. п/о №80"),
-      body: Container(),
+      body: Column(children:[
+        Container(
+          width: MediaQuery.of(context).size.width*0.9, 
+          height: 40,
+          decoration: BoxDecoration(border: Border.all(color: Colors.indigo[600])),
+          child: Center(child: Text('Наличие товара', style: TextStyle(fontSize: 18),))
+        ),
+        ListView.separated(
+          separatorBuilder: (context, index) => Divider(
+            color: Colors.black,
+          ),
+          shrinkWrap: true,
+          itemCount: array.length,
+          itemBuilder: (_, index) {
+            return Container(
+              margin: EdgeInsets.only(bottom: 0),
+              child: ListTile(
+                title: Text(array[index],
+                    style: TextStyle(fontSize: 15, color: Colors.black54)),
+                trailing: Text('1638 тг', style: TextStyle(fontSize: 15, color: Colors.black54)),
+                tileColor: Colors.white,
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MedicationCount()),);
+              }),
+            );
+          }),
+      ]),
     );
   }
 }
