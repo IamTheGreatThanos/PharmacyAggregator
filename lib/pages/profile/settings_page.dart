5c4b0ce91044f74d12f6162e123f68953862fb08
Dinmukhammed <iamthegreatthanos@gmail.com>
2021-03-14 20:52:28 +0600

    Added settings page.

--- lib/pages/profile/settings_page.dart
+++ lib/pages/profile/settings_page.dart
@@ -16,6 +16,8 @@ class _SettingsPageState extends State<SettingsPage> {
   TextEditingController nameController = TextEditingController();
   TextEditingController textController = TextEditingController();
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  bool isSwitched = false;
+  bool isSwitched2 = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,13 +35,37 @@ class _SettingsPageState extends State<SettingsPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-                  child: Text('Подтверждение подписи:', style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Montserrat Regular', fontWeight: FontWeight.bold)),
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical:10),
+                  child: Row(children: [
+                    Text('Отпечаток пальцев:', style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Montserrat Regular', fontWeight: FontWeight.bold)),
+                    Switch(
+                      value: isSwitched,
+                      onChanged: (value) {
+                        setState(() {
+                          isSwitched = value;
+                        });
+                      },
+                      activeTrackColor: Colors.red,
+                      activeColor: Colors.redAccent,
+                    ),
+                  ],),
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-                  child: Text('Вход:', style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Montserrat Regular', fontWeight: FontWeight.bold)),
-                ),
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(children: [
+                    Text('Уведомления:', style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'Montserrat Regular', fontWeight: FontWeight.bold)),
+                    Switch(
+                      value: isSwitched2,
+                      onChanged: (value) {
+                        setState(() {
+                          isSwitched2 = value;
+                        });
+                      },
+                      activeTrackColor: Colors.red,
+                      activeColor: Colors.redAccent,
+                    ),
+                  ],),
+                )
               ]
             )
           )
